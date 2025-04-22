0288e4c4b51ada08cc932f79dc76b9f0432795f0
M.Hayagreeva charyulu <163738076+hayagreeva-charyulu@users.noreply.github.com>
2025-04-22 11:50:08 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+import Sidebar from './components/sidebar/Sidebar'
+import MainSection from './components/mainSection/MainSection'
+import Footer from './components/footer/Footer'
+
+
+function App() {
+
+  return (
+    <>
+      <div className="mainContainer">
+        <Navbar />
+        <div className="mainSectionContentContainer">
+          <Sidebar />
+          <MainSection />
+        </div>
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
